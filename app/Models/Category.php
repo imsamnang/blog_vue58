773bd3d64f4a9276ededc5b1a2eb0fb5f4f36e6f
Samnang Im <applephagna@gmail.com>
2019-10-04 23:16:45 +0700

    create model Post & Category
    create model Post & Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+  public function posts()
+	{
+		return $this->hasMany(Post::class,'id');
+	}
 }
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['as'=>'admin.','prefix'=>'admin','namespace'=>'Admin'],function(){
+  Route::resource('posts','PostController');
+});
